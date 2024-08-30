1443ab9eb90ab7ffc194aaf2b735771fae9b7ffe
Prasanth K M <prasanthkm49@gmail.com>
2024-08-30 14:45:33 +0530

    api change

--- lib/env.dart
+++ lib/env.dart
@@ -1,5 +1,5 @@
-const apiEndPoint = "vertexlabs.online";
-// const apiEndPoint = "139.59.52.48:8069";
+const apiEndPoint = "erp.donekw.com";
+// const apiEndPoint = "vertexlabs.online";
 
 const clientId = "6xnfBLxO7cOyZ2tQeU7nQGWtT1zLXq";
 const clientSecret = "AmJVWEDv3Q8Je1ZmPdyiS7ydBJiPNW";
